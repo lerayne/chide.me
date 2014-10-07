33a1fcec6a6a8d7f536750822f35b18dd56212cb
lerayne <lerayne@gmail.com>
2014-10-07 14:55:11 +0300

    first dirty tests

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ chide.me
 ========
 
 Social service for anonymous critics and confession
+
+## Getting started with development
+
+First, to get all the libraries and dependancies you need to do [install bower](http://bower.io) and then do 
+` bower update ` in the repo's home directory.
+
+### Project runtime requirements:
+* **PHP** ~5.3
+* **MySQL** ~5
+
+### Dev requirements
+* **npm** (mostly to install bower)
+* **bower**
--- template.php
+++ template.php
@@ -20,10 +20,85 @@
 	<script src="<?= URL.LIB.'/lodash/dist/lodash.min.js' ?>" ></script>
 	<script src="<?= URL.LIB.'/platform/platform.js' ?>" ></script>
 
-	<link rel="import" href="<?= URL.LIB.'/polymer/polymer.html'?>">
+	<link rel="import" href="<?= URL.LIB.'/core-header-panel/core-header-panel.html'?>">
+	<link rel="import" href="<?= URL.LIB.'/core-toolbar/core-toolbar.html'?>">
+	<link rel="import" href="<?= URL.LIB.'/core-icons/core-icons.html'?>">
+	<link rel="import" href="<?= URL.LIB.'/paper-button/paper-button.html'?>">
+	<link rel="import" href="<?= URL.LIB.'/paper-tabs/paper-tabs.html'?>">
+	<link rel="import" href="<?= URL.LIB.'/paper-fab/paper-fab.html'?>">
+
+	<style>
+		body, html {
+			font-family: Calibri, Arial, sans-serif;
+		}
+
+		.content {
+			background: #fff;
+			margin: 60px 60px 60px 0;
+		}
+
+		core-toolbar paper-button {
+			font-size: 0.8em;
+			padding: 0.3em 0;
+			min-width: 2em;
+		}
+
+		core-toolbar {
+			height: auto;
+			background-color: #d01716;
+			color:white;
+		}
+
+		core-toolbar::shadow #bottomBar,
+		/*core-toolbar::shadow #middleBar,*/
+		core-toolbar::shadow #topBar
+		{
+			position: static;
+		}
+
+		core-toolbar::shadow .toolbar-tools {
+			height: 50px;
+		}
+
+		paper-tab {
+			font-weight: bold;
+			text-transform: uppercase;
+			cursor: pointer;
+		}
+
+		paper-tab::shadow #ink {
+			color:#ccc
+		}
+
+		paper-tabs::shadow #selectionBar {
+			background-color: #ccc;
+		}
+		
+		paper-fab {
+			background-color: #d01716;
+			position: fixed;
+			bottom:2em;
+			right:3em;
+		}
+	</style>
 
 </head>
-<body>
+<body fullbleed vertical layout>
+
+	<core-header-panel flex>
+		<core-toolbar>
+			<div class="bottom">
+				<div style="text-transform: uppercase; font-weight: bold"><span style="color:#ccc">chide</span>.me!</div>
+			</div>
+
+			<paper-tabs class="top" flex self-end selected="0">
+				<paper-tab>Dashboard</paper-tab>
+				<paper-tab>My chides</paper-tab>
+			</paper-tabs>
+		</core-toolbar>
+		<div class="content" style="height:2000px">Contents</div>
+		<paper-fab icon="add"></paper-fab>
+	</core-header-panel>
 
 </body>
 </html>
